8a493891fb75716b83ecb9718f41310b4b2e8710
vanajapulivarthi <54466610+vanajapulivarthi@users.noreply.github.com>
2022-10-17 14:10:46 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -23,7 +23,7 @@ parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 # sidebar for navigation
 with st.sidebar:
     
-    selected = option_menu('Multiple Disease Prediction System',
+    selected = option_menu('Applications of Machine Learning in the field of Medical Care',
                           
                           ['Diabetes Prediction',
                            'Heart Disease Prediction',
